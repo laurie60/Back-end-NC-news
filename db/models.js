17d4d6f24603be45e3e30d04825b1b109d39ecef
laurie60 <laurie8000@googlemail.com>
2022-07-05 15:45:23 +0100

    impliment update votes on articles

--- db/models.js
+++ db/models.js
@@ -22,4 +22,13 @@ exports.fetchArticleById = (articleId) => {
     });
 };
 
-//exports.alterVotes();
+exports.alterVotes = (articleId, inc_votes) => {
+  const queryStr =
+    "UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *;";
+  const variables = [inc_votes, articleId];
+  console.log(variables);
+  return db.query(queryStr, variables).then(({ rows }) => {
+    const updatedArticle = rows[0];
+    return updatedArticle;
+  });
+};
